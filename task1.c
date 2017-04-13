2aaf0ca328d6f1675b0c2d8cffc78f48f02f0d2e
hideyuki0121 <f.hideyuki@outlook.jp>
2017-04-14 01:24:30 +0900

    practice1-2

--- task1.c
+++ task1.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
  int main(void)
@@ -10,7 +11,19 @@
 
 	printf("15と37の差は%dです\a\a\a\n", 15 - 37);
 
-	printf("天\n地\n人");
+	printf("天\n地\n人\n");
 
+	int vx = 57;
+	int vy = vx + 10;
+
+	printf("vxの値は%dです\n", vx);
+	printf("vyの値は%dです\n", vy);
+
+	int xx = 3.14;
+	int yy = 5.7;
+
+	printf("%d\n", xx);
+	printf("%d\n", yy);
+	
 	return 0;
 }

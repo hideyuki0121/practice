126fef09636ec019c44cbfd372b5bdb26cfee314
hideyuki0121 <f.hideyuki@outlook.jp>
2017-04-13 19:05:24 +0900

    practice1-1

--- task1.c
+++ task1.c
@@ -2,7 +2,15 @@
 
  int main(void)
 {
-	printf("%d", 15 +37 );
+	printf("%d\n", 15 + 37 );
+
+	printf("%d\n", 15 - 37 );
+
+	printf("15と37の和は%dです\n", 15 + 37 );
+
+	printf("15と37の差は%dです\a\a\a\n", 15 - 37);
+
+	printf("天\n地\n人");
 
 	return 0;
 }

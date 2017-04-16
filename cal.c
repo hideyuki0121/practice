daf3d7ac1d67943b75fcd36a06ff78d74774fb77
hideyuki0121 <f.hideyuki@outlook.jp>
2017-04-17 02:39:33 +0900

    practice2-1

--- cal.c
+++ cal.c
@@ -1,17 +1,65 @@
+
 #include <stdio.h>
 
 int  main(void)
-{
-  int vx, vy;
-  puts("二つの整数を入力してください");
-  printf("整数vx:");   scanf("%d", &vx);
-  printf("整数vy:");   scanf("%d", &vy);
+{ /*
+    int vx, vy;
+    puts("二つの整数を入力してください");
+    printf("整数vx:");   scanf("%d", &vx);
+    printf("整数vy:");   scanf("%d", &vy);
 
-  printf("vx + vy = %d\n",vx + vy);
-  printf("vx - vy = %d\n",vx - vy);
-  printf("vx * vy = %d\n",vx * vy);
-  printf("vx / vy = %d\n",vx / vy);
-  printf("vx %% vy = %d\n",vx % vy);
-    
+    printf("vx + vy = %d\n",vx + vy);
+    printf("vx - vy = %d\n",vx - vy);
+    printf("vx * vy = %d\n",vx * vy);
+    printf("vx / vy = %d\n",vx / vy);
+    printf("vx %% vy = %d\n",vx % vy);
+  */
+  /*
+    int no;
+
+    printf("整数を入力してください:");
+    scanf("%d", &no);
+
+    printf("もっとも下の桁は%dです\n", no % 10);
+  */
+  /*
+    int a, b;
+
+    puts("二つの整数を入力してください");
+    printf("整数a:"); scanf("%d",&a );
+    printf("整数b:"); scanf("%d",&b );
+
+    printf("aをbで割ると%dあまり%dです\n",a / b, a % b);
+  */
+  /*
+  int x, y;
+
+  puts("二つの整数を入力してください");
+  printf("整数x:");   scanf("%d", &x );
+  printf("整数y:");   scanf("%d", &y );
+  
+  printf("xの値はyの%d%%です・\n", x * 100 / y);
+  
+  */
+  /*  
+  int A, B;
+
+  puts("二つの整数を入力してください");
+
+  printf("整数A:");   scanf("%d", &A );
+  printf("整数B:");   scanf("%d", &B );
+
+  printf("それらの和は%dで積は%dです。\n", A + B, A * B);
+  */
+
+    int num;
+
+    printf("整数を入力してください:");
+    scanf("%d", &num );
+
+    printf("符号を反転した値は%dです。\n", -num);
+  
+  
   return 0;
+				     
 }

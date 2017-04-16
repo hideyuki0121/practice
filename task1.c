43a18a4a63228feed50b3e3015f46c35dc484df7
hideyuki0121 <f.hideyuki@outlook.jp>
2017-04-17 02:55:36 +0900

    change some place

--- task1.c
+++ task1.c
@@ -3,13 +3,13 @@
 
  int main(void)
 {
-	printf("%d\n", 15 + 37 );
+	printf("%d", 15 + 37 );
 
-	printf("%d\n", 15 - 37 );
+	printf("%d", 15 - 37 );
 
-	printf("15と37の和は%dです\n", 15 + 37 );
+	printf("15と37の和は%dです", 15 + 37 );
 
-	printf("15と37の差は%dです\a\a\a\n", 15 - 37);
+	printf("15と37の差は%dです\a\a\a\n\n\n", 15 - 37);
 
 	printf("天\n地\n人\n");
 

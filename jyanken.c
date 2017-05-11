743a5a7a3086223aca915c858c78c4e904b0e7c8
hideyuki0121 <f.hideyuki@outlook.jp>
2017-05-11 20:50:17 +0900

    Add jyanken code

---
+++ jyanken.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+  srand((unsigned int)time(NULL));
+  
+  puts("welcome to the janken game!!");
+  
+
+  const int rock = 1;
+  const int scissors = 2;
+  const int paper = 3;
+  
+  int user_hand;
+  char input_hand[10];
+  user_hand =0;
+ 
+  while (user_hand == 0){
+    printf("please input your hand\n> ");
+    scanf("%s", input_hand);
+
+    if (!strcmp(input_hand,"rock"))user_hand = 1;
+    else if (!strcmp(input_hand,"scissors"))user_hand = 2;
+    else if (!strcmp(input_hand,"paper"))user_hand = 3;
+  }
+
+  int cpu_hand = (int)rand() % 3 + 1;
+
+  int judge = (user_hand - cpu_hand + 3) % 3;
+  
+  switch (judge){
+  case 0:
+    printf("user = %d : cpu = %d\n", user_hand, cpu_hand);
+    puts("DRAW!!");
+    break;
+  case 1:
+    printf("user = %d : cpu = %d\n", user_hand, cpu_hand);
+    puts("YOU LOSE");
+    break;
+  case 2:
+    printf("user = %d : cpu = %d\n", user_hand, cpu_hand);
+    puts("YOU WIN !!");
+  }
+  
+  
+  return 0;
+}

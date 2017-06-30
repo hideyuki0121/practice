7dbdb283f8f6e4c719b5a1aa335b1a123b6f6a0d
hideyuki0121 <f.hideyuki@outlook.jp>
2017-06-30 11:51:20 +0900

    finish prime number

--- c/prime_number.c
+++ c/prime_number.c
@@ -8,7 +8,22 @@ int div(int x, int y) //与えられた数字で割れるか判断
     return  t;
   else
     return f;
+}
 
+int judge(int a, int b) //素数判定器
+{
+  int i = 0;
+  while(b > 1){
+    if(div(a, b) == 1){
+      //    printf("%d  %d\n", a, b);
+      i++;
+    }
+    b--;
+  }
+  if(i == 0) //素数
+    return 1;
+  else
+    return 0;
 }
 
 int main(void)
@@ -18,22 +33,12 @@ int main(void)
   printf("please enter a number\n> ");
   scanf("%d", &n);
 
-  int a = n;
-  int b = n-1;
-  int i = 0; //公約数の数をカウント
-
-
-  while(b > 1){
-    if(div(a, b) == 1){
-      printf("%d  %d\n", a, b);
-      i++;
-    }
-    b--;
+  int i;
+  for(i = 1; n > i; n--){
+    int a = n;
+    int b = n-1;
+    if(judge( a, b) == 1)
+      printf("%d is a prime number!\n", a);
   }
-  if(i == 0)
-    printf("%d is a prime number!\n", a);
-  else
-    printf("\n");
-    
   return 0;
 }

84d5e400817eb3d4b6e0705b1341545a5a152690
hideyuki0121 <f.hideyuki@outlook.jp>
2017-08-27 21:44:14 +0900

    studying pointer and array

--- c_text/poi4.c
+++ c_text/poi4.c
@@ -7,19 +7,37 @@ void swap(int *px, int *py)//入れ替える関数
   *py = temp;
 }
 
+void sort(int *n1, int *n2)//比較する
+{
+  if(*n1 > *n2)
+    swap(n1, n2);
+}
+
+void sort3(int *n1, int *n2, int  *n3)
+{
+  if(*n1 > *n2)
+    swap(n1, n2);
+  if(*n2 > *n3)
+    swap(n2, n3);
+  if(*n1 > *n2)
+    swap(n1, n2);
+}
+
 int main(void)
 {
-  int na, nb;
+  int na, nb, nc;
 
   puts("２つの整数を入力してください");
   printf("整数A");   scanf("%d", &na);
   printf("整数B");   scanf("%d", &nb);
+  printf("整数C");   scanf("%d", &nc);
 
-  swap(&na, &nb);
+  sort3(&na, &nb, &nc);
 
-  puts("これらの値を交換しました");
+  puts("これらの値を小さい順にソートしました");
   printf("整数Aは%dです\n", na);
   printf("整数Bは%dです\n", nb);
+  printf("整数Cは%dです\n", nc);
 
   return 0;
 }
